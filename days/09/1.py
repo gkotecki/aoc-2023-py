9f6538652c57f0c4f61cbea5b88f3f6dc5a31281
gkotecki <gabrielkotecki@gmail.com>
2024-01-08 17:48:02 -0300

    ADD: day 09 done

---
+++ days/09/1.py
@@ -0,0 +1,40 @@
+import os
+import pprint
+from typing import List
+
+os.system("clear")
+os.system("cls")
+
+
+def pp(lines):
+    pprint.PrettyPrinter(indent=4).pprint(lines)
+
+
+lines = open("days/09/input.txt").read().splitlines()
+# lines = open("days/09/example.txt").read().splitlines()
+
+histories = [list(map(int, x.split())) for x in lines]
+
+pp(histories)
+
+
+def extrapolate(items: list[int]) -> int:
+    pp(f"extrapolate({items})")
+    diffs = []
+    all_zeroes = True
+    for i in range(len(items) - 1):
+        diff = items[i + 1] - items[i]
+        diffs.append(diff)
+        if diff != 0:
+            all_zeroes = False
+    if all_zeroes:
+        return items[-1]
+    return extrapolate(diffs) + items[-1]
+
+sum = 0
+for history in histories:
+    next = extrapolate(history)
+    print(next)
+    sum += next
+
+pp(sum)
